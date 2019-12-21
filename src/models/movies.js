421d6ce60b169308b293bd2610f52485467c8192
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2019-12-21 14:35:47 +0300

    Добавит в модель методы для фильтрации

--- src/models/movies.js
+++ src/models/movies.js
@@ -1,14 +1,29 @@
+import {getMoviesByFilter} from "../utils/filter.js";
+import {FilterType} from "../const.js";
+
 export default class Movies {
   constructor() {
     this._movies = [];
+    this._activeFilterType = FilterType.ALL;
+
+    this._filterChangeHandlers = [];
   }
 
   getMovies() {
+    return getMoviesByFilter(this._movies, this._activeFilterType);
+  }
+
+  getMoviesAll() {
     return this._movies;
   }
 
   setMovies(movies) {
     this._movies = Array.from(movies);
+  }
+
+  setFilter(filterType) {
+    this._activeFilterType = filterType;
+    this._filterChangeHandlers.forEach((handler) => handler());
   }
 
   updateMovie(id, movie) {
@@ -21,5 +36,9 @@ export default class Movies {
     this._movies = [].concat(this._movies.slice(0, index), movie, this._movies.slice(index + 1));
 
     return true;
+  }
+
+  setFilterChangeHandler(handler) {
+    this._filterChangeHandlers.push(handler);
   }
 }
