02c13b1870d526cd5356593d69761f689bad8b73
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-01-04 13:41:10 +0300

    Добавлен api.js

---
+++ src/api.js
@@ -0,0 +1,18 @@
+const Methods = {
+  GET: `GET`,
+  POST: `POST`,
+  PUT: `PUT`,
+  DELETE: `DELETE`
+};
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+export default class API {
+
+}
