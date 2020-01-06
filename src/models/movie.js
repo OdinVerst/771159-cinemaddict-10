016c542ba1b991f297f9a3d4f3c393b7c4fe92af
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-01-06 17:20:30 +0300

    Добавлены все поля в movie

--- src/models/movie.js
+++ src/models/movie.js
@@ -9,12 +9,25 @@ export default class Movie {
     this.id = data[`id`];
     this.comments = data[`comments`] || [];
 
-    if (data[Sections.INFO]) {
-      this.name = data[Sections.INFO].title;
-      this.alternaiveName = data[Sections.INFO].alternative_title;
-      this.poster = data[Sections.INFO].poster;
-    }
+    this.name = data[Sections.INFO].title;
+    this.alternaiveName = data[Sections.INFO].alternative_title;
+    this.rating = data[Sections.INFO].total_rating;
+    this.poster = data[Sections.INFO].poster;
+    this.age = data[Sections.INFO].age_rating;
+    this.director = data[Sections.INFO].director;
+    this.writers = new Set(data[Sections.INFO].writers);
+    this.actors = new Set(data[Sections.INFO].actors);
+    this.relaese = data[Sections.INFO].release.date;
+    this.contry = data[Sections.INFO].release.release_country;
+    this.duration = data[Sections.INFO].runtime;
+    this.genre = new Set(data[Sections.INFO].genre);
+    this.description = data[Sections.INFO].description;
 
+    this.userRating = data[Sections.USER].personal_rating;
+    this.isWatchlist = Boolean(data[Sections.USER].watchlist);
+    this.isWatched = Boolean(data[Sections.USER].already_watched);
+    this.userDateWatch = data[Sections.USER].watching_date;
+    this.isFavorite = Boolean(data[Sections.USER].favorite);
   }
 
   toRAW() {
